d33e944ddb4d232a5e8b99a25ebed14b09fdce86
aiman-khan <aimannkhn@gmail.com>
2024-03-02 20:47:36 +0500

    update

--- lib/features/quote/domain/data/auth_remote_datasource.dart
+++ lib/features/quote/domain/data/auth_remote_datasource.dart
@@ -1,6 +1,6 @@
-import 'package:calculator_flutter_app/features/quote/domain/usecases/get_random_photo_usecase.dart';
-import 'package:calculator_flutter_app/features/quote/domain/usecases/get_random_quote_usecase.dart';
-import 'package:calculator_flutter_app/infrastructure/datasource.dart';
+import 'package:diesel_powered/features/quote/domain/usecases/get_random_photo_usecase.dart';
+import 'package:diesel_powered/features/quote/domain/usecases/get_random_quote_usecase.dart';
+import 'package:diesel_powered/infrastructure/datasource.dart';
 
 abstract class AuthRemoteDataSource extends DataSource {
   Future<GetRandomPhotoUsecaseOutput> getRandomPhoto(
