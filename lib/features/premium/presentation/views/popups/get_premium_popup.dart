816e9960f1a783ba8cded9144b90173f00122025
aiman-khan <aimannkhn@gmail.com>
2024-03-02 20:45:46 +0500

    update

--- lib/features/premium/presentation/views/popups/get_premium_popup.dart
+++ lib/features/premium/presentation/views/popups/get_premium_popup.dart
@@ -1,9 +1,9 @@
-import 'package:calculator_flutter_app/common/extensions/num.dart';
-import 'package:calculator_flutter_app/common/widgets/app_text.dart';
-import 'package:calculator_flutter_app/common/widgets/back_button_widget.dart';
-import 'package:calculator_flutter_app/gen/assets.gen.dart';
-import 'package:calculator_flutter_app/util/resources/r.dart';
-import 'package:calculator_flutter_app/util/router/paths.dart';
+import 'package:diesel_powered/common/extensions/num.dart';
+import 'package:diesel_powered/common/widgets/app_text.dart';
+import 'package:diesel_powered/common/widgets/back_button_widget.dart';
+import 'package:diesel_powered/gen/assets.gen.dart';
+import 'package:diesel_powered/util/resources/r.dart';
+import 'package:diesel_powered/util/router/paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
