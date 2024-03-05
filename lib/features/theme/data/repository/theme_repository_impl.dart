49964bbd9086e9561fac078d8cfc818254e9f430
aiman-khan <aimannkhn@gmail.com>
2024-03-05 20:50:28 +0500

    update

--- lib/features/theme/data/repository/theme_repository_impl.dart
+++ lib/features/theme/data/repository/theme_repository_impl.dart
@@ -1,7 +1,7 @@
-import 'package:calculator_flutter_app/features/theme/domain/data/theme_local_data_source.dart';
-import 'package:calculator_flutter_app/features/theme/domain/repository/theme_repository.dart';
-import 'package:calculator_flutter_app/features/theme/domain/usecases/get_theme_usecase.dart';
-import 'package:calculator_flutter_app/features/theme/domain/usecases/switch_theme_usecase.dart';
+import 'package:diesel_powered/features/theme/domain/data/theme_local_data_source.dart';
+import 'package:diesel_powered/features/theme/domain/repository/theme_repository.dart';
+import 'package:diesel_powered/features/theme/domain/usecases/get_theme_usecase.dart';
+import 'package:diesel_powered/features/theme/domain/usecases/switch_theme_usecase.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: ThemeRepository)
