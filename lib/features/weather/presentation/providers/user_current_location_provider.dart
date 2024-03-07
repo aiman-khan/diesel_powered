72776d23f0cfc4f46d6c3b553ce31a91fc6f93d4
aiman-khan <aimannkhn@gmail.com>
2024-03-07 20:52:10 +0500

    update

--- lib/features/weather/presentation/providers/user_current_location_provider.dart
+++ lib/features/weather/presentation/providers/user_current_location_provider.dart
@@ -1,4 +1,4 @@
-import 'package:calculator_flutter_app/features/google_maps/domain/models/location/location_model.dart';
+import 'package:diesel_powered/features/google_maps/domain/models/location/location_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final userCurrentLocationProvider =
