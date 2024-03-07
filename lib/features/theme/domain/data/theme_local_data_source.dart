c09dcb81f2ec7ec2313ea546d7da7281cf3e0dfb
aiman-khan <aimannkhn@gmail.com>
2024-03-07 20:52:34 +0500

    update

--- lib/features/theme/domain/data/theme_local_data_source.dart
+++ lib/features/theme/domain/data/theme_local_data_source.dart
@@ -1,6 +1,6 @@
-import 'package:calculator_flutter_app/features/theme/domain/usecases/get_theme_usecase.dart';
-import 'package:calculator_flutter_app/features/theme/domain/usecases/switch_theme_usecase.dart';
-import 'package:calculator_flutter_app/infrastructure/datasource.dart';
+import 'package:diesel_powered/features/theme/domain/usecases/get_theme_usecase.dart';
+import 'package:diesel_powered/features/theme/domain/usecases/switch_theme_usecase.dart';
+import 'package:diesel_powered/infrastructure/datasource.dart';
 
 abstract class ThemeLocalDataSource extends DataSource {
   Future<SwitchThemeUsecaseOutput> switchTheme(SwitchThemeUsecaseInput input);
