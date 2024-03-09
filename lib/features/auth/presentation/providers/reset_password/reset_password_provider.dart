4b65a3709cbd18c765297de7418e9076faca8359
aiman-khan <aimannkhn@gmail.com>
2024-03-09 20:54:25 +0500

    update

--- lib/features/auth/presentation/providers/reset_password/reset_password_provider.dart
+++ lib/features/auth/presentation/providers/reset_password/reset_password_provider.dart
@@ -1,6 +1,6 @@
-import 'package:calculator_flutter_app/features/auth/domain/usecases/reset_password.dart';
-import 'package:calculator_flutter_app/features/auth/presentation/providers/reset_password/reset_password_form_provider.dart';
-import 'package:calculator_flutter_app/util/di/di.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/reset_password.dart';
+import 'package:diesel_powered/features/auth/presentation/providers/reset_password/reset_password_form_provider.dart';
+import 'package:diesel_powered/util/di/di.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'reset_password_provider.g.dart';
