2467e5bfce604276150d0e488ba9cffbd2be6464
aiman-khan <aimannkhn@gmail.com>
2024-03-09 20:54:10 +0500

    update

--- lib/features/calculator/presentation/views/calculate_item/widgets/calculation_form.dart
+++ lib/features/calculator/presentation/views/calculate_item/widgets/calculation_form.dart
@@ -1,9 +1,9 @@
-import 'package:calculator_flutter_app/common/extensions/num.dart';
-import 'package:calculator_flutter_app/common/widgets/app_filled_button.dart';
-import 'package:calculator_flutter_app/common/widgets/app_text.dart';
-import 'package:calculator_flutter_app/common/widgets/dialog_box.dart';
-import 'package:calculator_flutter_app/common/widgets/number_input_field.dart';
-import 'package:calculator_flutter_app/util/resources/r.dart';
+import 'package:diesel_powered/common/extensions/num.dart';
+import 'package:diesel_powered/common/widgets/app_filled_button.dart';
+import 'package:diesel_powered/common/widgets/app_text.dart';
+import 'package:diesel_powered/common/widgets/dialog_box.dart';
+import 'package:diesel_powered/common/widgets/number_input_field.dart';
+import 'package:diesel_powered/util/resources/r.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
