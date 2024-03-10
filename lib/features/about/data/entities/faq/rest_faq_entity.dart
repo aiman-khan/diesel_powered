909783b9686361b625fa5521cf3ed48d74302526
aiman-khan <aimannkhn@gmail.com>
2024-03-10 20:59:26 +0500

    update

--- lib/features/about/data/entities/faq/rest_faq_entity.dart
+++ lib/features/about/data/entities/faq/rest_faq_entity.dart
@@ -1,4 +1,4 @@
-import 'package:diesel_powered/features/about/domain/models/faq/faq_entity.dart';
+import 'package:calculator_flutter_app/features/about/domain/models/faq/faq_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'rest_faq_entity.freezed.dart';
