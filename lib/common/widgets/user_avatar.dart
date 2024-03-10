c3d9aef18231d94cd925093acbf3a53adbce6242
aiman-khan <aimannkhn@gmail.com>
2024-03-10 20:59:05 +0500

    update

--- lib/common/widgets/user_avatar.dart
+++ lib/common/widgets/user_avatar.dart
@@ -1,4 +1,4 @@
-import 'package:diesel_powered/util/resources/r.dart';
+import 'package:calculator_flutter_app/util/resources/r.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
