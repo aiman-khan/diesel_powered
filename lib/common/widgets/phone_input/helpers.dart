a82120acdd36c361623396bcd0eaa4fadc079f1d
aiman-khan <aimannkhn@gmail.com>
2024-03-11 21:00:50 +0500

    update

--- lib/common/widgets/phone_input/helpers.dart
+++ lib/common/widgets/phone_input/helpers.dart
@@ -1,4 +1,4 @@
-import 'package:diesel_powered/common/widgets/phone_input/countries.dart';
+import 'package:calculator_flutter_app/common/widgets/phone_input/countries.dart';
 
 bool isNumeric(String s) =>
     s.isNotEmpty && int.tryParse(s.replaceAll("+", "")) != null;
