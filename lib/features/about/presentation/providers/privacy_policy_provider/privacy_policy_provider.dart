7d8016d044fa4933cd75e38d50f694608b291a02
aiman-khan <aimannkhn@gmail.com>
2024-03-11 21:01:14 +0500

    update

--- lib/features/about/presentation/providers/privacy_policy_provider/privacy_policy_provider.dart
+++ lib/features/about/presentation/providers/privacy_policy_provider/privacy_policy_provider.dart
@@ -1,7 +1,7 @@
-import 'package:diesel_powered/features/about/domain/models/about/content_model.dart';
-import 'package:diesel_powered/features/about/domain/usecases/get_privacy_policy.dart';
-import 'package:diesel_powered/features/auth/presentation/providers/bearer_provider/bearer_provider.dart';
-import 'package:diesel_powered/util/di/di.dart';
+import 'package:calculator_flutter_app/features/about/domain/models/about/content_model.dart';
+import 'package:calculator_flutter_app/features/about/domain/usecases/get_privacy_policy.dart';
+import 'package:calculator_flutter_app/features/auth/presentation/providers/bearer_provider/bearer_provider.dart';
+import 'package:calculator_flutter_app/util/di/di.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'privacy_policy_provider.g.dart';
