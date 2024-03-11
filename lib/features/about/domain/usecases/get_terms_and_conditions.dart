3833a3f5185e1dfea9050982a0e60313c786e90e
aiman-khan <aimannkhn@gmail.com>
2024-03-11 21:01:02 +0500

    update

--- lib/features/about/domain/usecases/get_terms_and_conditions.dart
+++ lib/features/about/domain/usecases/get_terms_and_conditions.dart
@@ -1,9 +1,9 @@
-import 'package:diesel_powered/features/about/domain/models/about/content_entity.dart';
-import 'package:diesel_powered/features/about/domain/models/about/content_model.dart';
-import 'package:diesel_powered/features/about/domain/repository/about_repository.dart';
-import 'package:diesel_powered/infrastructure/usecase.dart';
-import 'package:diesel_powered/infrastructure/usecase_input.dart';
-import 'package:diesel_powered/infrastructure/usecase_output.dart';
+import 'package:calculator_flutter_app/features/about/domain/models/about/content_entity.dart';
+import 'package:calculator_flutter_app/features/about/domain/models/about/content_model.dart';
+import 'package:calculator_flutter_app/features/about/domain/repository/about_repository.dart';
+import 'package:calculator_flutter_app/infrastructure/usecase.dart';
+import 'package:calculator_flutter_app/infrastructure/usecase_input.dart';
+import 'package:calculator_flutter_app/infrastructure/usecase_output.dart';
 import 'package:injectable/injectable.dart';
 
 class GetTermsAndConditionsUsecaseInput extends Input {
