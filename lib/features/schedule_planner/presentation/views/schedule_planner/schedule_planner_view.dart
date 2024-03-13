ed9d331aca1ffdb85cb857c0625e566afc4ab64c
aiman-khan <aimannkhn@gmail.com>
2024-03-13 21:09:21 +0500

    update

--- lib/features/schedule_planner/presentation/views/schedule_planner/schedule_planner_view.dart
+++ lib/features/schedule_planner/presentation/views/schedule_planner/schedule_planner_view.dart
@@ -1,10 +1,10 @@
-import 'package:diesel_powered/common/extensions/num.dart';
-import 'package:diesel_powered/common/widgets/app_text.dart';
-import 'package:diesel_powered/common/widgets/back_button_widget.dart';
-import 'package:diesel_powered/features/schedule_planner/presentation/providers/task_provider.dart';
-import 'package:diesel_powered/features/schedule_planner/presentation/views/add_task/add_task_view.dart';
-import 'package:diesel_powered/features/schedule_planner/presentation/views/schedule_planner/widgets/year_picker_dialog.dart';
-import 'package:diesel_powered/util/resources/r.dart';
+import 'package:calculator_flutter_app/common/extensions/num.dart';
+import 'package:calculator_flutter_app/common/widgets/app_text.dart';
+import 'package:calculator_flutter_app/common/widgets/back_button_widget.dart';
+import 'package:calculator_flutter_app/features/schedule_planner/presentation/providers/task_provider.dart';
+import 'package:calculator_flutter_app/features/schedule_planner/presentation/views/add_task/add_task_view.dart';
+import 'package:calculator_flutter_app/features/schedule_planner/presentation/views/schedule_planner/widgets/year_picker_dialog.dart';
+import 'package:calculator_flutter_app/util/resources/r.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
