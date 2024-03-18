e5d7c57e5c3497ac1c23a5e4614d253922869eef
aiman-khan <aimannkhn@gmail.com>
2024-03-18 21:11:27 +0500

    update

--- lib/features/quote/data/entities/photo_url/rest_photo_url.dart
+++ lib/features/quote/data/entities/photo_url/rest_photo_url.dart
@@ -1,4 +1,4 @@
-import 'package:diesel_powered/features/quote/domain/models/photo_url/photo_url_entity.dart';
+import 'package:calculator_flutter_app/features/quote/domain/models/photo_url/photo_url_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'rest_photo_url.freezed.dart';
