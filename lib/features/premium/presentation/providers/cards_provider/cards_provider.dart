417f1fda1ccf24647f4261760066725d32729f88
aiman-khan <aimannkhn@gmail.com>
2024-03-21 21:11:52 +0500

    update

--- lib/features/premium/presentation/providers/cards_provider/cards_provider.dart
+++ lib/features/premium/presentation/providers/cards_provider/cards_provider.dart
@@ -1,5 +1,5 @@
-import 'package:diesel_powered/features/premium/domain/models/card/card_model.dart';
-import 'package:diesel_powered/gen/assets.gen.dart';
+import 'package:calculator_flutter_app/features/premium/domain/models/card/card_model.dart';
+import 'package:calculator_flutter_app/gen/assets.gen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final cardsProvider = StateProvider((ref) {
