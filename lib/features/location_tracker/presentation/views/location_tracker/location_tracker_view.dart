1aad92dfe093d56924f173278c62c54cd322c126
aiman-khan <aimannkhn@gmail.com>
2024-03-21 21:12:20 +0500

    update

--- lib/features/location_tracker/presentation/views/location_tracker/location_tracker_view.dart
+++ lib/features/location_tracker/presentation/views/location_tracker/location_tracker_view.dart
@@ -1,10 +1,10 @@
-import 'package:diesel_powered/common/extensions/num.dart';
-import 'package:diesel_powered/common/widgets/app_filled_button.dart';
-import 'package:diesel_powered/common/widgets/app_text.dart';
-import 'package:diesel_powered/common/widgets/back_button_widget.dart';
-import 'package:diesel_powered/common/widgets/text_input_field.dart';
-import 'package:diesel_powered/gen/assets.gen.dart';
-import 'package:diesel_powered/util/resources/r.dart';
+import 'package:calculator_flutter_app/common/extensions/num.dart';
+import 'package:calculator_flutter_app/common/widgets/app_filled_button.dart';
+import 'package:calculator_flutter_app/common/widgets/app_text.dart';
+import 'package:calculator_flutter_app/common/widgets/back_button_widget.dart';
+import 'package:calculator_flutter_app/common/widgets/text_input_field.dart';
+import 'package:calculator_flutter_app/gen/assets.gen.dart';
+import 'package:calculator_flutter_app/util/resources/r.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
