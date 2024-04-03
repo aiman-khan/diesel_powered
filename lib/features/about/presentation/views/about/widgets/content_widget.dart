f7fbe0c813342842362a013251a2e4c6e79daed5
aiman-khan <aimannkhn@gmail.com>
2024-04-03 21:18:19 +0500

    update

--- lib/features/about/presentation/views/about/widgets/content_widget.dart
+++ lib/features/about/presentation/views/about/widgets/content_widget.dart
@@ -1,7 +1,7 @@
-import 'package:diesel_powered/common/extensions/num.dart';
-import 'package:diesel_powered/common/widgets/app_text.dart';
-import 'package:diesel_powered/features/about/domain/models/about/content_model.dart';
-import 'package:diesel_powered/util/resources/r.dart';
+import 'package:calculator_flutter_app/common/extensions/num.dart';
+import 'package:calculator_flutter_app/common/widgets/app_text.dart';
+import 'package:calculator_flutter_app/features/about/domain/models/about/content_model.dart';
+import 'package:calculator_flutter_app/util/resources/r.dart';
 import 'package:flutter/material.dart';
 
 class ContentWidget extends StatelessWidget {
