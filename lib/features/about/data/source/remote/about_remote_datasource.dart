5aa1076119ea544b0d20808159fa1c6256842486
aiman-khan <aimannkhn@gmail.com>
2024-04-13 21:23:13 +0500

    update

--- lib/features/about/data/source/remote/about_remote_datasource.dart
+++ lib/features/about/data/source/remote/about_remote_datasource.dart
@@ -1,7 +1,7 @@
-import 'package:diesel_powered/features/about/domain/usecases/get_about.dart';
-import 'package:diesel_powered/features/about/domain/usecases/get_privacy_policy.dart';
-import 'package:diesel_powered/features/about/domain/usecases/get_terms_and_conditions.dart';
-import 'package:diesel_powered/infrastructure/datasource.dart';
+import 'package:calculator_flutter_app/features/about/domain/usecases/get_about.dart';
+import 'package:calculator_flutter_app/features/about/domain/usecases/get_privacy_policy.dart';
+import 'package:calculator_flutter_app/features/about/domain/usecases/get_terms_and_conditions.dart';
+import 'package:calculator_flutter_app/infrastructure/datasource.dart';
 
 abstract class AboutRemoteDataSource extends DataSource {
   Future<GetTermsAndConditionsUsecaseOutput> getTermsAndConditions(
