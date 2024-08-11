510bc534815b3ee9d6603e337fdf851b87dd0aa8
aiman-khan <aimannkhn@gmail.com>
2024-08-11 20:27:22 +0500

    update

--- lib/features/auth/domain/data/auth_remote_datasource.dart
+++ lib/features/auth/domain/data/auth_remote_datasource.dart
@@ -1,12 +1,12 @@
-import 'package:calculator_flutter_app/features/auth/domain/usecases/current_user.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/delete_user.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/initiate_reset_password_verification.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/login_driver.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/register_driver.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/reset_password.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/update_profile.dart';
-import 'package:calculator_flutter_app/features/auth/domain/usecases/verify_otp.dart';
-import 'package:calculator_flutter_app/infrastructure/datasource.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/current_user.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/delete_user.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/initiate_reset_password_verification.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/login_driver.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/register_driver.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/reset_password.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/update_profile.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/verify_otp.dart';
+import 'package:diesel_powered/infrastructure/datasource.dart';
 
 abstract class AuthRemoteDataSource extends DataSource {
   Future<LoginDriverUsecaseOutput> login(LoginDriverUsecaseInput input);
--- lib/features/auth/presentation/providers/reset_password/initiate_reset_password_verification_provider.dart
+++ lib/features/auth/presentation/providers/reset_password/initiate_reset_password_verification_provider.dart
@@ -1,6 +1,6 @@
-import 'package:calculator_flutter_app/features/auth/domain/usecases/initiate_reset_password_verification.dart';
-import 'package:calculator_flutter_app/features/auth/presentation/providers/reset_password/reset_password_form_provider.dart';
-import 'package:calculator_flutter_app/util/di/di.dart';
+import 'package:diesel_powered/features/auth/domain/usecases/initiate_reset_password_verification.dart';
+import 'package:diesel_powered/features/auth/presentation/providers/reset_password/reset_password_form_provider.dart';
+import 'package:diesel_powered/util/di/di.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'initiate_reset_password_verification_provider.g.dart';
